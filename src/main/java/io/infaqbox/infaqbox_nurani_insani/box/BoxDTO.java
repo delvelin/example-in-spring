76d1289d21819f13c464319630bb53387654d7d7
Hangga Aji Sayekti <bazeniancode@gmail.com>
2024-11-30 13:48:08 +0700

    update

--- src/main/java/io/infaqbox/infaqbox_nurani_insani/box/BoxDTO.java
+++ src/main/java/io/infaqbox/infaqbox_nurani_insani/box/BoxDTO.java
@@ -17,7 +17,6 @@ public class BoxDTO {
 
     private Integer idArea;
 
-<<<<<<< src/main/java/io/infaqbox/infaqbox_nurani_insani/box/BoxDTO.java
     @NotNull
     @Size(max = 255)
     private String namaTempat;
@@ -32,17 +31,4 @@ public class BoxDTO {
     @NotNull
     @Size(max = 255)
     private String cpName;
-=======
-    @Size(max = 255)
-    private String namaTempat;
-
-    private String alamat;
-
-    @Size(max = 255)
-    private String phone;
-
-    @Size(max = 255)
-    private String cp;
->>>>>>> src/main/java/io/infaqbox/infaqbox_nurani_insani/box/BoxDTO.java
-
 }
--- src/main/java/io/infaqbox/infaqbox_nurani_insani/user/UserService.java
+++ src/main/java/io/infaqbox/infaqbox_nurani_insani/user/UserService.java
@@ -68,13 +68,12 @@ public class UserService {
         return userRepository.existsByUsernameIgnoreCase(username);
     }
 
-<<<<<<< src/main/java/io/infaqbox/infaqbox_nurani_insani/user/UserService.java
     public boolean firstNameExists(final String firstName) {
         return userRepository.existsByFirstNameIgnoreCase(firstName);
-=======
-    public boolean completeNameExists(final String completeName) {
-        return userRepository.existsByCompleteName(completeName);
->>>>>>> src/main/java/io/infaqbox/infaqbox_nurani_insani/user/UserService.java
     }
+
+//    public boolean completeNameExists(final String completeName) {
+//        return userRepository.existsByCompleteName(completeName);
+//    }
 
 }
